f2962dcc9e016b3ce26e689c742b3a8062f73175
Tomasz Kisielewski <tomek.kisielewski@gmail.com>
2016-12-11 23:24:49 +0100

    Initial Commit

---
+++ maszyna/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  maszyna
+//
+//  Created by Tomasz Kisielewski on 11.12.2016.
+//  Copyright © 2016 Tomasz Kisielewski. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
