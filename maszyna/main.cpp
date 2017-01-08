15ec1f730df5ebe668da33ec1d622db26e27e2ae
Tomasz Kisielewski <tomek.kisielewski@gmail.com>
2017-01-08 02:37:39 +0100

    Wersja 0.2

--- maszyna/main.cpp
+++ maszyna/main.cpp
@@ -17,9 +17,10 @@ using namespace std;
 enum rejestry { r1 = 0, r2 = 1 };
 enum flaga { flU = -1, flZ = 0, flD = 1 };
 enum rozkaz { opDodaj = 0, opOdejmij = 1, opMnoz = 2, opDziel = 3, opPorownaj = 4, opKopiuj = 5, opSkocz = 6, opWczytaj = 7, opPobierz = 8, opWyswietl = 9, opZakoncz = 10, opZapisz = 11};
-enum menuGlowne { mgZakoncz = 0, mgOpis, mgTworz, mgWykonaj, mgPrzyklady };
+enum menuGlowne { mgZakoncz = 0, mgOpis, mgTworz, mgWykonajLiniowo, mgWykonajKrokowo, mgPrzyklady };
 enum menuPomoc { mpPowrot = 0, mpOperacje};
 enum menuOperacje { moPowrot = 0 };
+enum trybWykonywania { tLiniowo = 0, tKrokowo = 1 };
 
 const char szF[3][2] = { "U", "Z", "D" };
 const char szR[12][9] = { "dodaj"  , "odejmij" , "mnoz"   , "dziel"   ,
@@ -31,7 +32,7 @@ void wyswietlMenu();
 void wyswietlMenuSzczegoly();
 void wyswietlMenuOperacje();
 void wyswietlOperacje();
-void plikWykonaj(fstream &plik);
+void plikWykonaj(fstream &plik, int iTryb);
 void wyswietlPlik(fstream &plik);
 void plikTworz(fstream &plik);
 void plikZamknij(fstream &plik);
@@ -41,10 +42,8 @@ void plikSkok(fstream &plik, int &iSlowo, unsigned int &cntInstrukcja);
 void menuGlowne(fstream &plik);
 void menuSzczegoly(fstream &plik);
 void menuOperacje(fstream &plik);
-void wyswietlRozkaz(int rozkaz);
-void wyswietlRozkazInt(int rozkaz, int i);
-
-
+void wyswietlRozkaz(int iRozkaz);
+void wyswietlRozkazInt(int iRozkaz, int iSlowo);
 
 int main(int argc, const char * argv[])
 {
@@ -122,10 +121,16 @@ void menuGlowne(fstream &plik)
                 plikTworz(plik);
                 break;
             }
-            case mgWykonaj:
+            case mgWykonajLiniowo:
             {
                 while(plikOtworzWej(plik));
-                plikWykonaj(plik);
+                plikWykonaj(plik, tLiniowo);
+                break;
+            }
+            case mgWykonajKrokowo:
+            {
+                while(plikOtworzWej(plik));
+                plikWykonaj(plik, tKrokowo);
                 break;
             }
             case mgPrzyklady:
@@ -187,9 +192,9 @@ void plikTworz(fstream &plik)
     cout << "+----+----------+---------------------+---------------------------------------+" << endl;
     
     unsigned int cntInstrukcja = 0;
-    short int iRozkaz = 0;
-    short int iOperacja = 0;
-    short int itIndeks[2];
+    short iRozkaz = 0;
+    short iOperacja = 0;
+    short itIndeks[2];
     int iSlowo = 0;
     bool bBlad = true;
 
@@ -280,7 +285,8 @@ void plikTworz(fstream &plik)
                 while(bBlad)
                 {
                     cout << "Podaj wartosc skoku: " << endl << "> ";
-                    if(!cin >> iSlowo)
+                    cin >> iSlowo;
+                    if(cin.fail())
                     {
                         cout << "Bledna wartosc skoku!" << endl;
                     }
@@ -349,15 +355,15 @@ void plikTworz(fstream &plik)
     plikZamknij(plik);
 }
 
-void plikWykonaj(fstream &plik)
+void plikWykonaj(fstream &plik, int iTryb)
 {
     int iRejestr[64];
-    char iFlaga = flZ;
+    short iFlaga = flZ;
     unsigned int cntInstrukcja = 0;
-    short int iRozkaz = 0;
-    short int iOperacja = 0;
-    short int iGUI = 0;
-    short int itIndeks[2];
+    short iRozkaz = 0;
+    short iOperacja = 0;
+    short iGUI = 0;
+    short itIndeks[2];
     char ctRozkaz[2];
     char ctSlowo[4];
     int iSlowo = 0;
@@ -402,11 +408,14 @@ void plikWykonaj(fstream &plik)
         cout << "| ROZKAZ: "<<setw(11)<<iRozkaz<<" FLAGA: "<<szF[iFlaga+1]<<setw(11)<<s<<"KROK: "<<setw(11)<<cntInstrukcja<<setw(19)<<s<<"|"<<endl;
         cout << "| WCZYTANO:   "<<iWczytano<<" bajty"<<setw(20)<<s<<"KARETKA: "<<setw(8)<<plik.tellg()<<setw(19)<<s<<"|" << endl;
         cout << "+--------------------------------------+-------------------------------------+" << endl;
-        cout << "| 1. Wykonaj instrukcje                | 2. Zakoncz wykonywanie pliku        |" << endl;
-        cout << "+--------------------------------------+-------------------------------------+" << endl;
         
-        cin >> iGUI;
-        
+        if(iTryb == tKrokowo)
+        {
+            cout << "| 1. Wykonaj instrukcje                | 2. Zakoncz wykonywanie pliku        |" << endl;
+            cout << "+--------------------------------------+-------------------------------------+" << endl;
+            cin >> iGUI;
+        }
+    
         switch(iOperacja)
         {
             case opDodaj: // dodaj
@@ -548,7 +557,11 @@ void plikWykonaj(fstream &plik)
                 cout << iRejestr[itIndeks[r1]] << endl;
                 break;
             }
-            case opZakoncz: break;
+            case opZakoncz:
+            {
+                system("PAUSE");
+                break;
+            }
             default: break;
         }
     }
@@ -610,8 +623,9 @@ void wyswietlMenu()
     cout << "+---------------------------------MENU GLOWNE---------------------------------+" << endl;
     cout << "| 1. Szczegolowy opis Maszyny Wirtualnej                                      |" << endl;
     cout << "| 2. Tworzenie pliku programu Maszyny Wirtualnej                              |" << endl;
-    cout << "| 3. Wykonywanie programu Maszyny Wirtualnej                                  |" << endl;
-    cout << "| 4. Przykladowe programy Maszyny Wirtualnej                                  |" << endl;
+    cout << "| 3. Wykonywanie liniowe programu Maszyny Wirtualnej                          |" << endl;
+    cout << "| 4. Wykonywanie krokowe programu Maszyny Wirtualnej                          |" << endl;
+    cout << "| 5. Przykladowe programy Maszyny Wirtualnej                                  |" << endl;
     cout << "| 0. Zakoncz program Maszyna Wirtualna                                        |" << endl;
     cout << "+-----------------------------------------------------------------------------+" << endl;
     cout << "> ";
@@ -663,6 +677,106 @@ void wyswietlMenuOperacje()
     cout << "| 0. Powrot do MENU SZCZEGOLY                                                 |" << endl;
     cout << "+-----------------------------------------------------------------------------+" << endl;
     cout << "> ";
+}
+
+void wyswietlMenuPrzyklady()
+{
+    cout << "+--------------------------------MENU PRZYKLADY-------------------------------+" << endl;
+    cout << "| 1. Przyklad z zadania projektowego                                          |" << endl;
+    cout << "| 2. Dodawanie dwoch licz                                                     |" << endl;
+    cout << "| 3. Dzielenie 2 liczb                                                        |" << endl;
+    cout << "| 4. Najwieksza z trzech                                                      |" << endl;
+    cout << "| 0. Powrot do MENU GLOWNEGO                                                  |" << endl;
+    cout << "+-----------------------------------------------------------------------------+" << endl;
+    cout << "> ";
+}
+
+void wyswietlPrzyklad(int iKtory)
+{
+    switch(iKtory)
+    {
+        case 1:
+        {
+            cout << "+-----------------------PRZYKLAD Z ZADANIA PROJEKTOWEGO-----------------------+" << endl;
+            cout << "| Nazwa pliku: projekt.bin                                                    |" << endl;
+            cout << "+-----------------------------------------------------------------------------+" << endl;
+            cout << "| * Pobierz liczbe z klawiatury do rejestru 0 (0*2^4+8 = 8)                   |" << endl;
+            cout << "| * Porownaj rejestr 0 z rejestrem 2 (2*2^10+0*2^4+4 = 2052)                  |" << endl;
+            cout << "| * Skocz o 2 instrukcje do przodu jeśli nie zero (2*2^4+6 = 38 INT: 2)       |" << endl;
+            cout << "| * Skoncz program (10 = 10)                                                  |" << endl;
+            cout << "| * Do rejestru 1 dodaj rejestr 0, wynik do rejestru 1 (0*2^10+1*2^4+0 = 16)  |" << endl;
+            cout << "| * Wypisz rejestr 1 (1*2^4+9 = 25)                                           |" << endl;
+            cout << "| * Skocz o 6 instrukcji do tyłu (0*2^4+6 = 6)                                |" << endl;
+            cout << "+-----------------------------------------------------------------------------+" << endl;
+            cout << "| 0. Powrot do MENU PRZYKLADY                                                 |" << endl;
+            cout << "+-----------------------------------------------------------------------------+" << endl;
+            cout << "> ";
+            break;
+        }
+        case 2:
+        {
+            cout << "+----------------------------DODAWANIE DWOCH LICZB----------------------------+" << endl;
+            cout << "| Nazwa pliku: dod_ab.bin                                                     |" << endl;
+            cout << "+-----------------------------------------------------------------------------+" << endl;
+            cout << "| * Pobierz liczbe z klawiatury do rejestru 0 (0*2^4 + 8 = 8)                 |" << endl;
+            cout << "| * Pobierz liczbe z klawiatury do rejestru 1 (1*2^4 + 8 = 24)                |" << endl;
+            cout << "| * Do rejestru 0 dodaj rejestr 1 (1*2^10 + 0*2^4 + 0 = 1024)                 |" << endl;
+            cout << "| * Wypisz rejestr 0 (0*2^4 + 9 = 9)                                          |" << endl;
+            cout << "| * Skoncz program (10 = 10)                                                  |" << endl;
+            cout << "+-----------------------------------------------------------------------------+" << endl;
+            cout << "| 0. Powrot do MENU PRZYKLADY                                                 |" << endl;
+            cout << "+-----------------------------------------------------------------------------+" << endl;
+            cout << "> ";
+            break;
+        }
+        case 3:
+        {
+            cout << "+----------------------------DZIELENIE DWOCH LICZB----------------------------+" << endl;
+            cout << "| Nazwa pliku: dziel_ab.bin                                                   |" << endl;
+            cout << "+-----------------------------------------------------------------------------+" << endl;
+            cout << "| * Pobierz liczbe z klawiatury do rejestru 0 (0*2^4 + 8 = 8)                 |" << endl;
+            cout << "| * Pobierz liczbe z klawiatury do rejestru 1 (1*2^4 + 8 = 24)                |" << endl;
+            cout << "| * Podziel rejestr 0 przez rejestr 1 (1*2^10 + 0*2^4 + 3 = 1027)             |" << endl;
+            cout << "| * Wypisz rejestr 0 (0*2^4 + 9 = 9)                                          |" << endl;
+            cout << "| * Wypisz rejestr 1 (1*2^4 + 9 = 25)                                         |" << endl;
+            cout << "| * Skoncz program (10 = 10)                                                  |" << endl;
+            cout << "+-----------------------------------------------------------------------------+" << endl;
+            cout << "| 0. Powrot do MENU PRZYKLADY                                                 |" << endl;
+            cout << "+-----------------------------------------------------------------------------+" << endl;
+            cout << "> ";
+            break;
+        }
+        case 4:
+        {
+            cout << "+--------------------------------NAJWIEKSZA Z 3-------------------------------+" << endl;
+            cout << "| Nazwa pliku: max_3.bin                                                      |" << endl;
+            cout << "+-----------------------------------------------------------------------------+" << endl;
+            cout << "| * Pobierz liczbe z klawiatury do rejestru 0 (0*2^4 + 8 = 8)                 |" << endl;
+            cout << "| * Pobierz liczbe z klawiatury do rejestru 1 (1*2^4 + 8 = 24)                |" << endl;
+            cout << "| * Pobierz liczbe z klawiatury do rejestru 2 (2*2^4 + 8 = 40)                |" << endl;
+            cout << "| * Porownaj rejestr 0 z rejestrem 1 (1*2^10 + 0*2^4 + 4 = 1028)              |" << endl;
+            cout << "| * Skocz o 2 instrukcje do przodu jesli dodatnia (3*2^4 + 6 = 54 INT: 2)     |" << endl;
+            cout << "| * Skocz o 4 instrukcje do przodu jesli ujemna (4*2^4 + 6 = 70 INT: 4)       |" << endl;
+            cout << "| * Porownaj rejestr 0 z rejestrem 2 (2*2^10 + 0*2^4 + 4 = 2052)              |" << endl;
+            cout << "| * Skocz o 6 instrukcje do przodu jesli dodatnia (3*2^4 + 6 = 54 INT: 6)     |" << endl;
+            cout << "| * Skocz o 9 instrukcje do przodu jesli ujemna (4*2^4 + 6 = 70 INT: 9)       |" << endl;
+            cout << "| * Porownaj rejestr 1 z rejestrem 2 (2*2^10 + 1*2^4 + 4 = 2068)              |" << endl;
+            cout << "| * Skocz o 5 instrukcje do przodu jesli dodatnia (3*2^4 + 6 = 54 INT: 5)     |" << endl;
+            cout << "| * Skocz o 6 instrukcje do przodu jesli ujemna (4*2^4 + 6 = 70 INT: 6)       |" << endl;
+            cout << "| * Skoncz program (10 = 10)                                                  |" << endl;
+            cout << "| * Wypisz rejestr 0 (0*2^4 + 9 = 9)                                          |" << endl;
+            cout << "| * Skocz o 2 instrukcje do tylu (0*2^4 + 6 = 6 INT: -2)                      |" << endl;
+            cout << "| * Wypisz rejestr 1 (1*2^4 + 9 = 25)                                         |" << endl;
+            cout << "| * Skocz o 4 instrukcje do tylu (0*2^4 + 6 = 6 INT: -4)                      |" << endl;
+            cout << "| * Wypisz rejestr 2 (1*2^4 + 9 = 41)                                         |" << endl;
+            cout << "| * Skocz o 6 instrukcje do tylu (0*2^4 + 6 = 6 INT: -6)                      |" << endl;
+            cout << "+-----------------------------------------------------------------------------+" << endl;
+            cout << "| 0. Powrot do MENU PRZYKLADY                                                 |" << endl;
+            cout << "+-----------------------------------------------------------------------------+" << endl;
+            cout << "> ";
+            break;
+        }
+    }
 }
 
 void wyswietlRozkaz(int iRozkaz)
